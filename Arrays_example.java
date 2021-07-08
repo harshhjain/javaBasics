9be555c9d93356f6c3334f57eee571b61dc2f623
Harsh Vardhan Jain <66672238+harshhjain@users.noreply.github.com>
2021-07-08 23:16:16 +0530

    Update Arrays_example.java

--- Arrays_example.java
+++ Arrays_example.java
@@ -5,6 +5,9 @@ public class Main {
         var[0] = 10.0f;
         var[1] = 20.0f;
         var[2] = 50.0f;
+
+        // float[] var = {10.0f, 20.0f, 30.0f};
+
         float sum = 0.0f;
 
         for (int i=0;i < var.length;i++) sum += var[i];
