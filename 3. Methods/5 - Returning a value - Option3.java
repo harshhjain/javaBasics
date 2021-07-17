8b87ed82409862c9d66b3df1bad2a3097cb2d722
Harsh Vardhan Jain <66672238+harshhjain@users.noreply.github.com>
2021-07-18 00:06:46 +0530

    Create 5 - Returning a value - Option3.java

---
+++ 3. Methods/5 - Returning a value - Option3.java
@@ -0,0 +1,17 @@
+
+    public static void main(String[] args) {
+
+/*        double result = calculateInterest (100d, 5d, 10);
+        System.out.println("Interest = " + result);*/
+
+        System.out.println("Interest = " + calculateInterest(100d, 5d, 10));
+
+    }
+
+    static double calculateInterest(double p, double r, double t){
+    //    double interest = p * r * t;
+    //    return interest;
+    return p * r * t;
+    }
+
+}
